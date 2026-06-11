43b39830e869af1b407f53f910d5e7035881e17e
Michael Mark Moeller <michael.markmoeller@gmail.com>
2026-06-11 16:06:02 +0200

    Fix: add missing useEffect import in StationCard

--- src/components/StationCard.tsx
+++ src/components/StationCard.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react'
+import { useRef, useState, useEffect } from 'react'
 import { useSortable } from '@dnd-kit/sortable'
 import { CSS } from '@dnd-kit/utilities'
 import type { Station } from '../types'
