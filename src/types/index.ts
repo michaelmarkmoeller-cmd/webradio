52388047fc0601b689dfa241d01a841c6c4fdfb8
Michael Markmøller <michael.markmoeller@gmail.com>
2026-06-11 10:25:19 +0200

    Sort categories alphabetically

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,6 @@
 export type Category = "70's" | "80's" | "90's" | 'Pop' | 'Rock' | 'Dansk' | 'Italo' | 'Jul' | 'Dance'
 
-export const CATEGORIES: Category[] = ["70's", "80's", "90's", 'Pop', 'Rock', 'Dansk', 'Italo', 'Jul', 'Dance']
+export const CATEGORIES: Category[] = ["70's", "80's", "90's", 'Dance', 'Dansk', 'Italo', 'Jul', 'Pop', 'Rock']
 
 export interface Station {
   id: string
